4588434dac836f5b815b3947a78433d01a32163c
Abolfazl Ghorbani <ab.ghorbani28@gmail.com>
2022-06-29 13:01:36 +0200

    Create Basic Example Navbar

---
+++ src/data.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {FaBehance, FaFacebook, FaLinkedin, FaTwitter} from 'react-icons/fa';
+
+export const links = [
+     {
+          id: 1,
+          url: '/',
+          text: 'Bücher',
+     },
+       {
+          id: 2,
+          url: '/',
+          text: 'CDs & Vinyl',
+     },
+       {
+          id: 3,
+          url: '/',
+          text: 'DVDs',
+     },
+       {
+          id: 4,
+          url: '/',
+          text: 'Spiele',
+     }
+     
+]
+
+export const socials = [
+     {
+          id: 1,
+          url: 'https://twitter.com',
+          icon: <FaTwitter />
+     },
+     {
+          id: 2,
+          url: 'https://twitter.com',
+          icon: <FaBehance />
+     },
+     {
+          id: 3,
+          url: 'https://twitter.com',
+          icon: <FaFacebook />
+     },
+     {
+          id: 4,
+          url: 'https://twitter.com',
+          icon: <FaLinkedin />
+     },
+] 
